0fdb8203f0571ee7ade1c2f07ab1b62c5a7c59f4
jinsujj <wlstncjs1234@naver.com>
2023-06-12 17:33:50 +0900

    init 세팅

---
+++ src/main/java/com/example/trendyTracker/TrendyTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.trendyTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendyTrackerApplication {
+	// http://localhost:8080/swagger-ui/index.html
+	public static void main(String[] args) {
+		SpringApplication.run(TrendyTrackerApplication.class, args);
+	}
+
+}
