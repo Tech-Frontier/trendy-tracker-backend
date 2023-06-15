2fe95dada8657fcdbcb4b17e7a1dfdbb649b14f9
Jinsu <wlstncjs1234@naver.com>
2023-06-15 13:38:43 +0900

    Dev (#2)
    * gitignore 추가
    
    * QueryDsl 종속성 추가
    
    * app_info 테이블 생성 및 뼈대 설정
    
    * api controller 설정
    
    * 불필요한 Legacy 삭제
    
    * feed back 반영

---
+++ src/main/java/com/trendyTracker/api/AppInfo/AppInfoController.java
@@ -0,0 +1,74 @@
+package com.trendyTracker.api.AppInfo;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.trendyTracker.common.response.Response;
+import com.trendyTracker.service.AppInfoService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Tag(name = "App Info", description = "앱 정보 API")
+@RequiredArgsConstructor
+@RequestMapping("/api/appInfo")
+@RestController
+public class AppInfoController {
+    private final AppInfoService appInfoService;
+
+    @Operation(summary = "health check")
+    @GetMapping("/health-check")
+    public String getHealthCheck() {
+        return "ok";
+    }
+
+    @Operation(summary = "서비스 버전 조회")
+    @GetMapping("/get-version")
+    public Response<String> getAppVersion() {
+        String appVersion = appInfoService.getAppInfo().getVersion();
+
+        return Response.success(200, "버전이 정상 조회되었습니다.", appVersion);
+    }
+
+    @Operation(summary = "서비스 약관 조회")
+    @GetMapping("/get-term")
+    public Response<String> getAppTerms() {
+        String appTerm = appInfoService.getAppInfo().getTerms();
+
+        return Response.success(200, "약관이 정상 조회되었습니다", appTerm);
+    }
+
+    @Operation(summary = "서비스 정보 입력")
+    @PostMapping("/save")
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "앱 정보 등록이 완료되었습니다.") })
+    public Response<Void> SaveAppInfo(@RequestBody @Validated appInfoRequest appInfoRequest) {
+        String version = appInfoRequest.getVersion();
+        String terms = appInfoRequest.getTerms();
+
+        appInfoService.saveAppInfo(version, terms);
+
+        return Response.success(200, "서비스 정보 등록이 완료되었습니다.", null);
+    }
+
+    @Data
+    static class appInfoRequest {
+        @NotNull
+        @Schema(description = "버전", example = "1.0", type = "String")
+        private String version;
+
+        @NotNull
+        @Schema(description = "약관", example = "약관", type = "String")
+        private String terms;
+
+    }
+}
---
+++ src/main/java/com/trendyTracker/repository/AppInfoRepositoryImpl.java
@@ -0,0 +1,41 @@
+package com.trendyTracker.repository;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.trendyTracker.domain.AppInfo;
+import com.trendyTracker.domain.QAppInfo;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class AppInfoRepositoryImpl implements AppInfoRepository {
+
+    private final EntityManager em;
+    private JPAQueryFactory queryFactory;
+
+    @Override
+    public Optional<AppInfo> getAppInfo() {
+        queryFactory = new JPAQueryFactory(em);
+
+        QAppInfo qAppInfo = QAppInfo.appInfo;
+        var result = queryFactory.selectFrom(qAppInfo)
+                .orderBy(qAppInfo.version.desc())
+                .fetchFirst();
+
+        return Optional.ofNullable(result);
+    }
+
+    @Override
+    public void saveAppInfo(String version, String terms) {
+        AppInfo newAppInfo = new AppInfo();
+        newAppInfo.addAppInfo(version, terms);
+
+        em.persist(newAppInfo);
+    }
+
+}
