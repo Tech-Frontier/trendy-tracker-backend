ba9f385140ba8e65ef7cb171cc6f3198a9a22814
jinsujj <wlstncjs1234@naver.com>
2023-07-24 22:25:46 +0900

    Custom Exception 정의

---
+++ src/main/java/com/trendyTracker/common/Exception/ExceptionDetail/AlreadyExistException.java
@@ -0,0 +1,8 @@
+package com.trendyTracker.common.Exception.ExceptionDetail;
+
+public class AlreadyExistException extends Exception {
+    public AlreadyExistException(String message) {
+        super(message);
+    }
+
+}
