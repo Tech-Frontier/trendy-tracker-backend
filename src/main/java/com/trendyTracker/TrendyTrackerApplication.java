24f50170e3875216343149ad8855e268083d27f6
jinsujj <wlstncjs1234@naver.com>
2023-07-28 01:29:40 +0900

    서버 구동 시, 기준 테크 조회하도록 실행

--- src/main/java/com/trendyTracker/TrendyTrackerApplication.java
+++ src/main/java/com/trendyTracker/TrendyTrackerApplication.java
@@ -1,15 +1,27 @@
 package com.trendyTracker;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import com.trendyTracker.service.TechService;
+
+import lombok.RequiredArgsConstructor;
+
 @SpringBootApplication
 @EnableJpaAuditing
-public class TrendyTrackerApplication {
-    // http://localhost:8080/swagger-ui/index.html
+@RequiredArgsConstructor
+public class TrendyTrackerApplication implements CommandLineRunner{
+    private final TechService techService;
 
     public static void main(String[] args) {
         SpringApplication.run(TrendyTrackerApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("get TechList!.");
+        techService.getTechList();
     }
 }
