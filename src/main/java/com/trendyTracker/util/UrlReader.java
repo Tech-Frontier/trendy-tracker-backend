4bae83bf27c31b96169b9eea35675564cd6169fb
jinsujj <wlstncjs1234@naver.com>
2023-07-28 01:29:04 +0900

    url 파싱 기능 부분 구현

--- src/main/java/com/trendyTracker/util/UrlReader.java
+++ src/main/java/com/trendyTracker/util/UrlReader.java
@@ -1,19 +1,39 @@
 package com.trendyTracker.util;
 
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.springframework.http.ResponseEntity;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.web.client.RestTemplate;
 
 public class UrlReader {
     static RestTemplate restTemplate = new RestTemplate();
 
-    public static String getUrlContent(String url) {
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+    public static Set<String> getUrlContent(String url) throws IOException {
+        // 기준 정보 Tech 추출
+        TechListSingleton techListSingleton = TechListSingleton.getInstance();
+        List<String> techList = techListSingleton.getTechList();
+        Set<String> result = new HashSet<>();
 
-        String htmlString = response.getBody();
-        Document document = Jsoup.parse(htmlString);
+        Document doc = Jsoup.connect(url).get();
+        Elements div = doc.select("div");
+        // div 조각 탐
+        for (Element element : div) {
+            String contents = element.text();
 
-        return document.text();
+            System.out.println(contents);
+            // 추출된 단어 출력 또는 원하는 작업 수행
+            for (String tech : techList) {
+                if(contents.toLowerCase().contains(tech.toLowerCase()))
+                    result.add(tech);
+            }
+        }
+
+        return result;
     }
 }
