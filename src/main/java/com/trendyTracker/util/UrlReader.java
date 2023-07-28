0be71b065f75fb0cc27d7882a02ca2c10ca9a73c
Jinsu <wlstncjs1234@naver.com>
2023-07-29 00:23:29 +0900

    Feature/selenium (#17)
    * selenium 으로 웹 스크래핑
    
    * gradle 수정
    
    * 테스트 코드 수정
    
    * 파싱하는 부분 수정
    
    * 크롬 드라이버 설치 경로 변경
    
    * 임시 테스트 주석처리
    
    * 자잘한 코드 수정

--- src/main/java/com/trendyTracker/util/UrlReader.java
+++ src/main/java/com/trendyTracker/util/UrlReader.java
@@ -1,40 +1,53 @@
 package com.trendyTracker.util;
 
 import java.io.IOException;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import org.springframework.web.client.RestTemplate;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class UrlReader {
-    static RestTemplate restTemplate = new RestTemplate();
-
-    // TODO 제대로 동작하게 수정 필요
     public static Set<String> getUrlContent(String url) throws IOException {
-        // 기준 정보 Tech 추출
         TechListSingleton techListSingleton = TechListSingleton.getInstance();
         List<String> techList = techListSingleton.getTechList();
-        Set<String> result = new HashSet<>();
 
-        Document doc = Jsoup.connect(url).get();
-        Elements div = doc.select("div");
-        // div 조각 탐
-        for (Element element : div) {
-            String contents = element.text();
+        // [ MacOS ]
+        // System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        // [ RaspberryPi ]
+        System.setProperty("webdriver.chrome.driver", "/usr/lib/chromium-browser/chromedriver");
+        ChromeOptions options = new ChromeOptions().addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
+        
+        try {
+            driver.get(url);
+            WebElement bodyElement = driver.findElement(By.tagName("body"));
+            String pageSource = bodyElement.getText();
 
-            System.out.println(contents);
-            // 추출된 단어 출력 또는 원하는 작업 수행
-            for (String tech : techList) {
-                if(contents.toLowerCase().contains(tech.toLowerCase()))
-                    result.add(tech);
-            }
+            // 영어 단어를 추출하는 정규 표현식
+            String regex = "\\b[a-zA-Z]+\\b";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(pageSource);
+            List<String> englishWords = new ArrayList<>();
+
+            while (matcher.find()) 
+                englishWords.add(matcher.group());
+
+            return techList.stream()
+                .filter(tech -> englishWords.stream()
+                .anyMatch(word -> tech.equalsIgnoreCase(word)))
+                .collect(Collectors.toSet());
+
+        } finally {
+            // 드라이버 종료
+            driver.quit();
         }
-
-        return result;
     }
 }
