146fffaca4b41c5f1d5168c8f13c17028c8b510f
jinsujj <wlstncjs1234@naver.com>
2023-07-28 18:16:51 +0900

    테스트 코드 수정

--- src/test/java/com/trendyTracker/util/UrlReaderTest.java
+++ src/test/java/com/trendyTracker/util/UrlReaderTest.java
@@ -6,21 +6,24 @@ import java.util.Set;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class UrlReaderTest {
 
     @Test
     @DisplayName("url 정보 파싱")
     public void getUrlContent() throws Exception {
         // given
-        var testUrl = "https://www.owl-dev.me/blog/68";
+        var testUrl = "https://www.owl-dev.me/blog/62";
 
         // when
         Set<String> urlContent = UrlReader.getUrlContent(testUrl);
 
         // then
-        Assertions.assertThat(urlContent.contains("Substation API 성능 개선기 (ORM vs SQL) 2편"));
-        Assertions.assertThat(urlContent.contains("SQL과 ORM에 대해서 파고들고,"));
+        Assertions.assertThat(urlContent.contains("Java")).isTrue();
+        
 
     }
 }
