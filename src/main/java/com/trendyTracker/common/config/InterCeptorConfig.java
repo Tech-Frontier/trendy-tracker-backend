274b41ec641599bb399a30e4663971e2f1aa0e6b
jinsujj <wlstncjs1234@naver.com>
2023-08-12 15:52:11 +0900

    어드민 API 토큰 로직 삽입

---
+++ src/main/java/com/trendyTracker/common/config/InterCeptorConfig.java
@@ -0,0 +1,24 @@
+package com.trendyTracker.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterCeptorConfig implements WebMvcConfigurer{
+    @Value("${token.value}")
+    private String tokenValue;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        // JWT 토큰 검증을 적용할 URL 패턴 지정
+        registry.addInterceptor(new JwtInterceptor(tokenValue))
+            // Tech
+            .addPathPatterns("/api/tech/stack/create")
+            .addPathPatterns("/api/tech/stack/delete")
+            // Recruit
+            .addPathPatterns("/api/recruit/regist");         
+
+    }
+}
