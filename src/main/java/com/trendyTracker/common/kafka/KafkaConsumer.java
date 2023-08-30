55cb77c5cfe4047304bb9c419da6351799f97bbe
Jinsu <wlstncjs1234@naver.com>
2023-08-30 21:46:08 +0900

    logstash 체계 구현 (#32)
    * uopdate
    
    * update
    
    * update logstash
    
    * update docker-compose

--- src/main/java/com/trendyTracker/common/kafka/KafkaConsumer.java
+++ src/main/java/com/trendyTracker/common/kafka/KafkaConsumer.java
@@ -7,17 +7,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "logger", groupId = "my-group")
-    public void listenToTopic1(ConsumerRecord<String, String> record) {
-        String key = record.key();
-        String value = record.value();
-        System.out.println("Received message - Key: " + key + ", Value: " + value);
-    }
+    // @KafkaListener(topics = "logger", groupId = "my-group")
+    // public void listenToTopic1(ConsumerRecord<String, String> record) {
+    //     String key = record.key();
+    //     String value = record.value();
+    //     System.out.println("Received message - Key: " + key + ", Value: " + value);
+    // }
 
-    @KafkaListener(topics = "error", groupId = "my-group")
-    public void listenToTopic2(ConsumerRecord<String, String> record) {
-        String key = record.key();
-        String value = record.value();
-        System.out.println("Received message - Key: " + key + ", Value: " + value);
-    }
+    // @KafkaListener(topics = "error", groupId = "my-group")
+    // public void listenToTopic2(ConsumerRecord<String, String> record) {
+    //     String key = record.key();
+    //     String value = record.value();
+    //     System.out.println("Received message - Key: " + key + ", Value: " + value);
+    // }
 }
