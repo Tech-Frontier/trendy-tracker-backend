077e8194d7555acc0390aba1a1da9b8e2a8f2029
jinsujj <wlstncjs1234@naver.com>
2023-09-09 10:46:59 +0900

    기존 jwtInterceptor 의 자잘한 주석 제거

--- src/main/java/com/trendyTracker/common/config/JwtInterceptor.java
+++ src/main/java/com/trendyTracker/common/config/JwtInterceptor.java
@@ -14,19 +14,19 @@ public class JwtInterceptor implements HandlerInterceptor {
     
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
-
-        // JWT 토큰 검증 로직 구현
+    /**
+     *  validate admin token 
+     */
         String token = request.getHeader("Authorization");
+
         if (token != null && token.startsWith("Bearer ")) {
             String jwtToken = token.substring(7);
-
-            // Admin 토큰
-            if (jwtToken.equals(adminToken)) return true;
+            if (jwtToken.equals(adminToken)) 
+                return true;
 
             return false;
         }
         else {
-            // JWT 토큰이 없는 경우 처리
             response.setStatus(HttpServletResponse.SC_FORBIDDEN);
             return false;
         }
