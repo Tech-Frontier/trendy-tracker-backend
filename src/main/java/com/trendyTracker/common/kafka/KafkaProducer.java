5899e8d5bf3684020ce57a4ad1ca837b00d2d5dc
jinsujj <wlstncjs1234@naver.com>
2023-09-09 10:45:46 +0900

    kafka produce 에서 header 정보도 수집하도록 변경

--- src/main/java/com/trendyTracker/common/kafka/KafkaProducer.java
+++ src/main/java/com/trendyTracker/common/kafka/KafkaProducer.java
@@ -1,6 +1,9 @@
 package com.trendyTracker.common.kafka;
 
+import java.nio.charset.StandardCharsets;
+
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.internals.RecordHeader;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +15,12 @@ public class KafkaProducer {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    public void sendMessage(String topic, String key, String value) {
-        ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
+    public void sendMessage(String topic, String key, String value, String header) {
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, null, key, value);
+
+        RecordHeader recordHeader = new RecordHeader("uuid", header.getBytes(StandardCharsets.UTF_8));
+        record.headers().add(recordHeader);
+
         kafkaTemplate.send(record);
-    }   
+    }
 }
