085d16381b361d7f4e9b860e450d1aa6f5b7b89b
jinsujj <wlstncjs1234@naver.com>
2023-09-13 01:39:11 +0900

    채용공고 등록 API 이벤트 기반으로 변경

--- src/main/java/com/trendyTracker/common/kafka/KafkaProducer.java
+++ src/main/java/com/trendyTracker/common/kafka/KafkaProducer.java
@@ -1,11 +1,15 @@
 package com.trendyTracker.common.kafka;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.header.internals.RecordHeader;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class KafkaProducer {
@@ -16,11 +20,29 @@ public class KafkaProducer {
     }
 
     public void sendMessage(String topic, String key, String value, String header) {
+    /*
+     * Logging   
+     */
         ProducerRecord<String, String> record = new ProducerRecord<>(topic, null, key, value);
 
         RecordHeader recordHeader = new RecordHeader("uuid", header.getBytes(StandardCharsets.UTF_8));
         record.headers().add(recordHeader);
 
+        kafkaTemplate.send(record);
+    }
+
+    public void sendMessage(String topic, Map<String,String> messageMap, String header) throws JsonProcessingException{
+    /*
+     * 채용공고 등록
+     */
+        ObjectMapper objectMapper = new ObjectMapper();
+        String message = objectMapper.writeValueAsString(messageMap);
+
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, message);
+
+        RecordHeader recordHeader = new RecordHeader("uuid", header.getBytes(StandardCharsets.UTF_8));
+        record.headers().add(recordHeader);
+        
         kafkaTemplate.send(record);
     }
 }
