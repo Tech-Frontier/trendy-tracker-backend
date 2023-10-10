9df95b260cc67920cceb5489631849b2bc5b2a68
jinsujj <wlstncjs1234@naver.com>
2023-10-10 23:18:32 +0900

    messaging Adapter 형태로 변경
    (cherry picked from commit 78d9aa6d65f27a132aabf50a53292ec641b7bd8b)

--- src/main/java/com/trendyTracker/Job/repository/KafkaConsumerImpl.java
+++ src/main/java/com/trendyTracker/Job/repository/KafkaConsumerImpl.java
@@ -1,5 +1,7 @@
 package com.trendyTracker.Job.repository;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -13,52 +15,67 @@ import org.springframework.stereotype.Service;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.trendyTracker.Job.service.RecruitService;
+import com.trendyTracker.infrastructure.kafka.KafkaProducer;
+
 
 @Service
 public class KafkaConsumerImpl implements MessageConsumer<ConsumerRecord<String, String>> {
     @Autowired
     private RecruitService recruitService;
+    @Autowired
+    private KafkaProducer kafkaProducer;
+
     static Logger logger = LoggerFactory.getLogger(KafkaConsumerImpl.class);
 
     @Override
     @KafkaListener(topics = "RegistJob", groupId = "job-regist", concurrency = "3")
-    public void registJobUrl(ConsumerRecord<String, String> mesage, @Header(name = "uuid") Optional<String> header) {
+    public void registJobUrl(ConsumerRecord<String, String> mesage, @Header(name = "uuid") Optional<String> header) throws Exception {
         /*
          * 채용공고 등록 event
          */
-        try{
-            String value = mesage.value();
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(value);
-            String url = jsonNode.get("url").asText();
-            String company = jsonNode.get("company").asText();
-            String occupation = jsonNode.get("occupation").asText();
-
-
-            long id = recruitService.regisitJobPostion(url, company, occupation);
-            logger.info("Consumed RegistJob Topic: id:" + id + " header: " + header.get());
+        JsonNode jsonNode = new ObjectMapper().readTree(mesage.value());
+        String url = jsonNode.get("url").asText();
+        String company = jsonNode.get("company").asText();
+        String occupation = jsonNode.get("occupation").asText();
+        String uuid = header.get();
         
-        }catch (Exception ex){
+        try{
+            long id = recruitService.regisitJobPostion(url, company, occupation);
+            logger.info("Consumed RegistJob Topic: id:" + id + " header: " + uuid);
+        
+        }
+        catch (Exception ex){
+            Map<String,String> params = new HashMap<>();
+            params.put("url", url);
+            params.put("company", company);
+            params.put("occupation", occupation);
+            params.put("uuid", uuid);
+            kafkaProducer.sendMessage("error", params, uuid);
+
             logger.error(header.get(), ex.getMessage());
         }
     }
 
     @Override
-    @KafkaListener(topics = "UpdateJob", groupId = "job-update", concurrency = "1")
-    public void updateJobUrl(ConsumerRecord<String, String> mesage, @Header(name = "uuid") Optional<String> header) {
+    @KafkaListener(topics = "UpdateJob", groupId = "job-update")
+    public void updateJobUrl(ConsumerRecord<String, String> mesage, @Header(name = "uuid") Optional<String> header) throws Exception {
         /*
          * 채용 공고 수정 event
          */
+        JsonNode jsonNode = new ObjectMapper().readTree(mesage.value());
+        String url = jsonNode.get("url").asText();
+        String uuid = header.get();
+        
         try{
-            String value = mesage.value();
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(value);
-            String url = jsonNode.get("url").asText();
-
             long id = recruitService.updateJobPosition(url);
-            logger.info("Consumed UpdateJob Topic: id:" + id + " header: " + header.get());
+            logger.info("Consumed UpdateJob Topic: id:" + id + " header: " + uuid);
         }
         catch (Exception ex){
+            Map<String,String> params = new HashMap<>();
+            params.put("url", url);
+            params.put("uuid", uuid);
+            kafkaProducer.sendMessage("error",params, uuid);
+
             logger.error(header.get(), ex.getMessage());
         }
     }
@@ -68,5 +85,4 @@ public class KafkaConsumerImpl implements MessageConsumer<ConsumerRecord<String,
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'deleteJobUrl'");
     }
-
 }
