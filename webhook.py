6392f8c41e4a3e7eb4e7ecb976dc914330d96c9c
jinsujj <wlstncjs1234@naver.com>
2023-12-18 22:22:08 +0900

    blue green 방식으로 배포

--- webhook.py
+++ webhook.py
@@ -2,6 +2,8 @@ import os
 import logging
 from flask import Flask
 import subprocess
+import requests
+import time
 
 app = Flask(__name__)
 
@@ -17,24 +19,25 @@ def handle_webhook():
         password = os.getenv("PASSWORD")
 
         # Docker 이미지 정보
-        logging.info(
-            f"********* docker image info **********\n{repository}\n{'*' * 40}"
-        )
+        logging.info(f"********* docker image **********\n{repository}\n{'*' * 40}")
+
         # Docker hub 로그인
         login_command = f"docker login --username='{username}' --password='{password}'"
         run_subprocess(login_command)
 
-        # Docker 이미지 Pull
+        # 새로운 이미지 Pull
         pull_command = f"docker pull {repository}"
         run_subprocess(pull_command)
 
-        # Docker 이미지 Down
-        down_command = "docker-compose stop trendy_tracker"
-        run_subprocess(down_command)
+        # 첫번째 Spring 프로젝트 update
+        stop_and_start_service()
 
-        # Docker 이미지 Run
-        run_command = "docker-compose -f ./docker-compose.yml up -d"
-        run_subprocess(run_command)
+        # 첫 번째 Spring 프로젝트의 상태를 폴링하여 확인
+        check_health("http://localhost:8080/api/appInfo/health-check")
+
+        # 두 번째 Spring 프로젝트 시작
+        start_command2 = "docker-compose -f ./docker-compose.yml up -d trendy_tracker2"
+        run_subprocess(start_command2)
 
         logging.info(f"Docker 컨테이너 빌드 및 실행 완료: {repository}")
         return f"Docker 컨테이너 빌드 및 실행 완료: {repository}", 200
@@ -45,18 +48,44 @@ def handle_webhook():
 
 
 def run_subprocess(command):
-    logging.info(f"********** 실행 명령어 **********\n{command}\n{'*' * 40}")
-    result = subprocess.run(
-        command,
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        encoding="utf-8",
+    logging.info(f"************************\n{command}\n{'*' * 40}")
+    process = subprocess.Popen(
+        command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
-    if result.returncode != 0:
-        error_message = f"명령어 실행 중 오류 발생: {result.stderr}"
-        logging.error(error_message)
-        raise Exception(error_message)
+    output, error = process.communicate()
+    if process.returncode != 0:
+        raise Exception(f"실행 오류: {error.decode()}")
+
+
+def stop_and_start_service():
+    # 첫 번째 Spring 프로젝트 중지
+    stop_command = "docker-compose stop trendy_tracker"
+    run_subprocess(stop_command)
+
+    # 첫 번째 Spring 프로젝트 다시 시작
+    start_command = "docker-compose -f ./docker-compose.yml up -d trendy_tracker"   
+    run_subprocess(start_command)
+
+
+def check_health(api_url):
+    max_attempts = 60  # 최대 시도 횟수
+    interval_seconds = 5  # 폴링 간격 (초)
+
+    for _ in range(max_attempts):
+        try:
+            response = requests.get(api_url)
+            if response.status_code == 200:
+                # API 정상 응답을 받으면 종료
+                logging.info("API 정상 동작 확인")
+                return
+        except Exception as e:
+            logging.warning(f"API 폴링 중 오류 발생: {str(e)}")
+
+        # 정상 응답이 아닌 경우 잠시 대기 후 다시 시도
+        time.sleep(interval_seconds)
+
+    # 최대 시도 횟수를 넘어갈 경우 예외 처리
+    raise Exception("API가 정상적으로 동작하지 않습니다.")
 
 
 if __name__ == "__main__":
