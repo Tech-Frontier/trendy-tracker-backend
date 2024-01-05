c57750fc9db7c137cbe70f69502efe24d977d424
jinsujj <wlstncjs1234@naver.com>
2024-01-05 17:31:28 +0900

    기술 스택별 월별 건수 와 퍼센트

--- src/main/java/com/trendyTracker/Domain/Jobs/Statistics/StaticsRepository.java
+++ src/main/java/com/trendyTracker/Domain/Jobs/Statistics/StaticsRepository.java
@@ -1,13 +1,18 @@
 package com.trendyTracker.Domain.Jobs.Statistics;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
 import com.trendyTracker.Domain.Jobs.Statistics.Dto.ChartInfo;
+import com.trendyTracker.Domain.Jobs.Statistics.Dto.TrendInfo;
+import com.trendyTracker.Domain.Jobs.Techs.Tech;
 import com.trendyTracker.Domain.Jobs.Techs.Tech.TechType;
 
 @Repository
 public interface StaticsRepository {
     ChartInfo getTechChart(LocalDate fromDate, LocalDate toDate, TechType techType);
+
+    List<TrendInfo> getTrendInfo(Tech tech);
 }
--- src/main/java/com/trendyTracker/Domain/Jobs/Statistics/StaticsService.java
+++ src/main/java/com/trendyTracker/Domain/Jobs/Statistics/StaticsService.java
@@ -1,10 +1,15 @@
 package com.trendyTracker.Domain.Jobs.Statistics;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.trendyTracker.Common.Exception.ExceptionDetail.NoResultException;
 import com.trendyTracker.Domain.Jobs.Statistics.Dto.ChartInfo;
+import com.trendyTracker.Domain.Jobs.Statistics.Dto.TrendInfo;
+import com.trendyTracker.Domain.Jobs.Techs.Tech;
+import com.trendyTracker.Domain.Jobs.Techs.TechRepository;
 import com.trendyTracker.Domain.Jobs.Techs.Tech.TechType;
 
 import lombok.RequiredArgsConstructor;
@@ -12,10 +17,18 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class StaticsService {
-
+    private final TechRepository techRepository;
     private final StaticsRepository staticsRepository;
 
     public ChartInfo getTechCharts(LocalDate fromDate, LocalDate toDate, TechType techType){
       return staticsRepository.getTechChart(fromDate, toDate, techType);
+    }
+
+    
+    public List<TrendInfo> getTrends(String techName) throws NoResultException {
+        Tech tech = techRepository.findById(techName)
+              .orElseThrow(() -> new NoResultException(techName + " 해당 기술 스택이 존재하지 않습니다"));
+
+        return staticsRepository.getTrendInfo(tech);
     }
 }
