0a88e0a0fcf79ff9a44152147423d0513a779d8f
jinsujj <wlstncjs1234@naver.com>
2024-03-26 22:01:29 +0900

    패키지 소문자화

--- src/main/java/com/trendytracker/Domain/AppService/UserSubscribeCompanies/UserSubscribeCompaniesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.trendytracker.domain.appservice.usersubscribecompanies;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserSubscribeCompaniesRepository extends JpaRepository<UserSubscribeCompanies, Long>{
-    
-}
--- src/main/java/com/trendytracker/Domain/AppService/UserSubscribeTemplates/UserSubscribeTemplatesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.trendytracker.domain.appservice.usersubscribetemplates;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserSubscribeTemplatesRepository extends JpaRepository<UserSubscribeTemplates, Long>{
-    
-}
